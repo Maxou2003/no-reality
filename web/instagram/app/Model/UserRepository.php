04986ac546968ebdd977b23a7de1ba29b68d5386
Maxence <lemaila.maxence@gmail.com>
2025-01-29 17:18:31 +0100

    fetchFollowers

--- web/instagram/app/Model/UserRepository.php
+++ web/instagram/app/Model/UserRepository.php
@@ -4,40 +4,6 @@ namespace App\Model;
 
 use App\Lib\DatabaseConnection;
 use App\Model\Entity\User;
-
-class UserRepository
-{
-    public DatabaseConnection $connection;
-
-    public function fetchFollowers($user_id): array
-    {
-        $statement = $this->connection->getConnection()->prepare(
-            'SELECT follower_id, user_firstname, user_lastname, user_description, user_username, user_pp_path FROM users u join subscription s on u.user_id=f.followed_id where f.user_id=:user_id'
-        );
-        $statement->bindValue(':user_id', $user_id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        $followersArray = [];
-        while (($row = $statement->fetch())) {
-            $follower = new User();
-            $follower->user_id = $row['user_id'];
-            $follower->user_username = $row['user_username'];
-            $follower->user_pp_path = $row['user_pp_path'];
-            $follower->user_firstname = $row['user_firstname'];
-            $follower->user_lastname = $row['user_lastname'];
-            $follower->user_description = $row['user_description'];
-
-            $followersArray[] = $follower;
-        }
-
-        return $followersArray;
-    }
-}
-<?php
-
-namespace App\Model;
-
-use App\Lib\DatabaseConnection;
 use App\Model\Entity\Post;
 use DateTime;
 
@@ -78,8 +44,9 @@ class UserRepository
     public function getUser($id): array
     {
         $statement = $this->connection->getConnection()->prepare(
-            'SELECT user_id, user_username, user_email, user_pp_path, user_bio, user FROM users WHERE user_id = :id'
+            'SELECT user_id,user_username,user_pp_path,user_firstname,user_lastname,user_description FROM users WHERE user_id = :id'
         );
+
         $statement->bindValue(':id', $id, \PDO::PARAM_INT);
         $statement->execute();
 
@@ -88,14 +55,38 @@ class UserRepository
             $user = new User();
             $user->user_id = $row['user_id'];
             $user->user_username = $row['user_username'];
-            $user->user_email = $row['user_email'];
             $user->user_pp_path = $row['user_pp_path'];
-            $user->user_bio = $row['user_bio'];
-            $user->user = $row['user'];
+            $user->user_firstname = $row['user_firstname'];
+            $user->user_lastname = $row['user_lastname'];
+            $user->user_description = $row['user_description'];
 
             $userArray[] = $user;
         }
 
         return $userArray;
+    }
+
+    public function fetchFollowers($user_id): array
+    {
+        $statement = $this->connection->getConnection()->prepare(
+            'SELECT follower_id, user_firstname, user_lastname, user_description, user_username, user_pp_path FROM users u join subscriptions s on u.user_id=s.follower_id where s.followed_id=:user_id'
+        );
+        $statement->bindValue(':user_id', $user_id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        $followersArray = [];
+        while (($row = $statement->fetch())) {
+            $follower = new User();
+            $follower->user_id = $row['user_id'];
+            $follower->user_username = $row['user_username'];
+            $follower->user_pp_path = $row['user_pp_path'];
+            $follower->user_firstname = $row['user_firstname'];
+            $follower->user_lastname = $row['user_lastname'];
+            $follower->user_description = $row['user_description'];
+
+            $followersArray[] = $follower;
+        }
+
+        return $followersArray;
     }
 }
