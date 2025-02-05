f07dd46a3ae920c59a582d8a9acb380346a6d9e9
Maxence <lemaila.maxence@gmail.com>
2025-02-05 13:57:03 +0100

    search followers/followings

--- web/instagram/public/js/post_view.js
+++ web/instagram/public/js/post_view.js
@@ -56,12 +56,64 @@ function openModalFollow(followers) {
     escapeModal();
 }
 
+function search() {
+    const searchBar = document.querySelector('.search-container input');
+    const follow_modal_body = document.querySelector(".follow-modal-body");
+    const followersBtn = document.getElementById("follow-btn");
+    const userId = followersBtn.getAttribute("data-user-id");
+    let headerdiv = document.querySelector(".follow-modal-header h2");
+    let apiUrl = "";
+
+    if (headerdiv.innerHTML == "Followers") {
+        apiUrl = `/no-reality/web/instagram/public/index.php?p=profile/searchInFollowers&user_id=${userId}&searchContent=${searchBar.value}&follow=1`
+    } else {
+        apiUrl = `/no-reality/web/instagram/public/index.php?p=profile/searchInFollowers&user_id=${userId}&searchContent=${searchBar.value}&follow=0`;
+    }
+
+    if (searchBar.value === "") {
+        follow_modal_body.innerHTML = "";
+
+    } else {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                follow_modal_body.innerHTML = ""; // Clear previous content
+
+                data.forEach(user => {
+                    const userElement = document.createElement("div");
+                    userElement.classList.add("follower-item");
+                    userElement.innerHTML = `
+                    <div class="follow-modal-profile">
+                        <div class="follow-modal-profile-img">
+                            <img src="${PROFILE_IMG_PATH}${user.profile_picture}" alt="Image">
+                        </div>
+                        <div class="follow-modal-profile-info">
+                            <a class="nav-link" href="${MY_URL}profile/${user.user_id}">
+                                <span class="user-name">${user.username}</span>
+                            </a>
+                            <span class="user-full-name">${user.firstname} ${user.lastname} </span>
+                        </div>
+                        <button class="follow-btn">Follow</button>
+                    </div>
+                    `;
+                    follow_modal_body.appendChild(userElement);
+                });
+
+            })
+            .catch(error => console.error("Error fetching followers:", error));
+    }
+
+}
+
+
 function closeModal() {
     const heartButtons = document.querySelectorAll('.heart_icon');
     for (let i = 0; i < heartButtons.length; i++) {
         heartButtons[i].childNodes[1].name = 'heart-outline';
         heartButtons[i].className = 'heart_icon';
     }
+    const searchBar = document.querySelector('.search-container input');
+    searchBar.value = '';
     const modal = document.getElementById('post-modal');
     modal.style.display = 'none';
     const modal_follow = document.getElementById('post-modal-follow');
