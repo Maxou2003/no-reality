e7ec61ad2a79de99ae6a1de77dab7528772e7a70
Maxence <lemaila.maxence@gmail.com>
2025-02-26 17:25:26 +0100

    db_update

--- backup-db/nr_instagram.sql
+++ backup-db/nr_instagram.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 13 fév. 2025 à 14:45
+-- Généré le : mer. 26 fév. 2025 à 17:25
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -100,7 +100,9 @@ CREATE TABLE `identification` (
 
 INSERT INTO `identification` (`identification_id`, `post_id`, `user_id`, `instance_id`) VALUES
 (1, 1, 2, 1),
-(2, 5, 2, 1);
+(2, 5, 2, 1),
+(3, 6, 4, 2),
+(4, 7, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -121,7 +123,8 @@ CREATE TABLE `instance` (
 --
 
 INSERT INTO `instance` (`instance_id`, `average_age`, `gender_prop`, `population`, `subject`) VALUES
-(1, 34, 1, 1, 'l\'eau chaude');
+(1, 34, 1, 1, 'test'),
+(2, 34, 1, 1, 'test2');
 
 -- --------------------------------------------------------
 
@@ -151,7 +154,9 @@ INSERT INTO `posts` (`post_id`, `user_id`, `instance_id`, `nb_likes`, `nb_views`
 (2, 1, 1, 24, 65, '2025-01-29 17:33:30', 'pexels-filatova-1861817299-30427823.jpg', 'Petite photo de zinzin !', 'Finlande, Helsinki (non je dahek)', 1),
 (3, 2, 1, 0, 0, '2025-01-29 17:55:52', 'finlande.jpg', 'mon voyage en Finlande', 'Finlande, Helsinki', 3),
 (4, 2, 1, 43, 67, '2025-02-12 07:51:43', 'pexels-kaboompics-6256.jpg', 'Un evier...', 'France, Angers', 0),
-(5, 1, 1, 4, 53, '2025-02-12 07:53:27', 'pexels-ps-photography-14694-67184.jpg', 'Ce robinet est meilleur !', 'Finlande, Helsinki', 1);
+(5, 1, 1, 4, 53, '2025-02-12 07:53:27', 'pexels-ps-photography-14694-67184.jpg', 'Ce robinet est meilleur !', 'Finlande, Helsinki', 1),
+(6, 5, 2, 140, 0, '2025-02-26 16:26:00', 'japan_1_0.jpg', 'Mes vacances au Japon, trop une dingz ! ', 'Japon', 0),
+(7, 4, 2, 2000000, 0, '2025-02-26 16:59:32', 'japan_1_4.jpg', 'I love Japan ! ', 'Japon', 0);
 
 -- --------------------------------------------------------
 
@@ -217,15 +222,18 @@ DELIMITER ;
 CREATE TABLE `subscriptions` (
   `subscription_id` int(11) NOT NULL,
   `follower_id` int(11) NOT NULL,
-  `followed_id` int(11) NOT NULL
+  `followed_id` int(11) NOT NULL,
+  `instance_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `subscriptions`
 --
 
-INSERT INTO `subscriptions` (`subscription_id`, `follower_id`, `followed_id`) VALUES
-(1, 1, 2);
+INSERT INTO `subscriptions` (`subscription_id`, `follower_id`, `followed_id`, `instance_id`) VALUES
+(1, 1, 2, 1),
+(2, 4, 5, 2),
+(3, 5, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -244,7 +252,11 @@ CREATE TABLE `userlinkinstance` (
 --
 
 INSERT INTO `userlinkinstance` (`link_id`, `user_id`, `instance_id`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 4, 2),
+(5, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -268,7 +280,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `user_username`, `user_firstname`, `user_lastname`, `user_pp_path`, `user_description`) VALUES
 (1, 'MaxLambert', 'Maxime', 'Lambert', 'maxime_lambert.jpg', 'Hey, je suis Maxime Lambert !\nBienvenue sur mon Instagram !\nJ\'aime le handball, et les jeux de sociétés !'),
 (2, 'Siphy666', 'Alexis', 'Paquereau--Gasnier', '1711984249368.jpg', 'Hey la team ! Comment va ?'),
-(3, 'Algo', 'Alain', 'Godon', 'ID_alain_polytech_NB_max203x270.jpg', 'Otp leona plat sur la faille de l\'invocateur');
+(3, 'Algo', 'Alain', 'Godon', 'ID_alain_polytech_NB_max203x270.jpg', 'Otp leona plat sur la faille de l\'invocateur'),
+(4, 'LeGoat', 'Martin', 'Mollat', '1727454408757.jpg', 'Théâtre d\'impro ce soir ouvert à tous !'),
+(5, 'PlotG', 'Gaston', 'Plot', '1636488595977.jpg', 'J\'aime beaucoup les restaurants chinois à volonté ! ');
 
 --
 -- Index pour les tables déchargées
@@ -319,7 +333,8 @@ ALTER TABLE `response`
 ALTER TABLE `subscriptions`
   ADD PRIMARY KEY (`subscription_id`),
   ADD KEY `FK_followerid` (`follower_id`),
-  ADD KEY `FK_followedid` (`followed_id`);
+  ADD KEY `FK_followedid` (`followed_id`),
+  ADD KEY `FK_SubscriptionInstance` (`instance_id`);
 
 --
 -- Index pour la table `userlinkinstance`
@@ -344,25 +359,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `identification`
 --
 ALTER TABLE `identification`
-  MODIFY `identification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `identification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `instance`
 --
 ALTER TABLE `instance`
-  MODIFY `instance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `instance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `response`
@@ -374,19 +389,19 @@ ALTER TABLE `response`
 -- AUTO_INCREMENT pour la table `subscriptions`
 --
 ALTER TABLE `subscriptions`
-  MODIFY `subscription_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `subscription_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `userlinkinstance`
 --
 ALTER TABLE `userlinkinstance`
-  MODIFY `link_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `link_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Contraintes pour les tables déchargées
@@ -425,6 +440,7 @@ ALTER TABLE `response`
 -- Contraintes pour la table `subscriptions`
 --
 ALTER TABLE `subscriptions`
+  ADD CONSTRAINT `FK_SubscriptionInstance` FOREIGN KEY (`instance_id`) REFERENCES `instance` (`instance_id`),
   ADD CONSTRAINT `FK_followedid` FOREIGN KEY (`followed_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `FK_followerid` FOREIGN KEY (`follower_id`) REFERENCES `users` (`user_id`);
 
