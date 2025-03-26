c2c37c9b376f50c47af082da240caa8e90b30729
AlexisPG <Siphy666@gmail.com>
2025-03-26 17:05:30 +0100

    first view for facebook profile

---
+++ web/facebook/app/Controller/ProfileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Lib\DatabaseConnection;
+use App\Model\PostRepository;
+use App\Model\UserRepository;
+
+class ProfileController
+{
+    private $twig;
+
+    public function __construct()
+    {
+        global $twig;
+        $this->twig = $twig;
+    }
+
+    public function main()
+    {
+        $database = new DatabaseConnection();
+        $UserRepository = new UserRepository();
+        $UserRepository->connection = $database;
+
+        $slug = strval($_GET['username']);
+        $user_id = $UserRepository->getUserIdBySlug($slug);
+        if ($user_id == -1) {;
+            http_response_code(404);
+            echo "Error 404: username '$slug' not found.";
+        } else {
+            $user = $UserRepository->getUser($user_id);
+            $posts = $UserRepository->getPosts($user_id);
+            $photos = $UserRepository->getPhotos($user_id);
+            // $followers_stats = $UserRepository->getUserFollowersStats($user_id);
+
+            $template = $this->twig->load('profile.twig');
+
+            echo $template->render(['posts' => $posts, 'user' => $user, 'photos' => $photos, 'URL' => URL, 'POST_IMG_PATH' => POST_IMG_PATH, 'PROFILE_IMG_PATH' => PROFILE_IMG_PATH, /*'followers_stats' => $followers_stats,*/ 'nbPosts' => count($posts)]);
+        }
+    }
+}
